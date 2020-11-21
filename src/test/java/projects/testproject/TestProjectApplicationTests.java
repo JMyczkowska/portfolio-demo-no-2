d5db75112f0dac80a53b58236fb74a3c434f75e0
Joanna Myczkowska <bluekomadori@gmail.com>
2020-11-21 11:27:12 +0100

    structure :: initial project structure

---
+++ src/test/java/projects/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
